9f6900d5a1e1e24ca2950baf8e350e94bea6831e
Carlos Scheidegger <carlos.scheidegger@posit.co>
2025-09-22 09:41:32 -0500

    add qmd writer entry point

--- crates/wasm-qmd-parser/src/lib.rs
+++ crates/wasm-qmd-parser/src/lib.rs
@@ -19,10 +19,10 @@ mod utils;
 
 use std::{io, panic};
 
-use quarto_markdown_pandoc::readers::qmd;
+use quarto_markdown_pandoc::readers;
 use quarto_markdown_pandoc::utils::output::VerboseOutput;
 use quarto_markdown_pandoc::utils::tree_sitter_log_observer::TreeSitterLogObserver;
-use quarto_markdown_pandoc::writers::json;
+use quarto_markdown_pandoc::writers;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -41,16 +41,31 @@ pub fn run() {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
 }
 
+fn pandoc_to_json(doc: &quarto_markdown_pandoc::pandoc::Pandoc) -> Result<String, String> {
+    let mut buf = Vec::new();
+    match writers::json::write(doc, &mut buf) {
+        Ok(_) => {
+            // Nothing to do
+        }
+        Err(err) => {
+            return Err(format!("Unable to write as json: {:?}", err));
+        }
+    }
+
+    match String::from_utf8(buf) {
+        Ok(json) => Ok(json),
+        Err(err) => Err(format!("Unable to convert json to string: {:?}", err)),
+    }
+}
+
 #[wasm_bindgen]
 pub fn parse_qmd(input: JsValue) -> JsValue {
     let mut output = VerboseOutput::Sink(io::sink());
-
     let input = match input.as_string() {
         Some(input) => input,
         None => panic!("Unable to parse `input` as a `String`."),
     };
-
-    let result = match qmd::read(
+    let result = match readers::qmd::read(
         input.as_bytes(),
         false,
         "<input>",
@@ -61,23 +76,21 @@ pub fn parse_qmd(input: JsValue) -> JsValue {
         Err(err) => panic!("Unable to read as a qmd:\n{}", err.join("\n")),
     };
 
-    let mut buf = Vec::new();
+    let json = pandoc_to_json(&result).unwrap();
+    JsValue::from_str(&json)
+}
 
-    match json::write(&result, &mut buf) {
-        Ok(_) => {
-            // Nothing to do
-        }
-        Err(err) => {
-            panic!("Unable to write as json: {:?}", err)
-        }
-    }
-
-    let json = match String::from_utf8(buf) {
-        Ok(json) => json,
-        Err(err) => {
-            panic!("Unable to convert json to string: {:?}", err)
-        }
+#[wasm_bindgen]
+pub fn write_qmd(input: JsValue) -> JsValue {
+    let input = match input.as_string() {
+        Some(input) => input,
+        None => panic!("Unable to parse `input` as a `String`."),
+    };
+    let result = match readers::json::read(&mut input.as_bytes()) {
+        Ok(result) => result,
+        Err(err) => panic!("Unable to read as json:\n{}", err),
     };
 
+    let json = pandoc_to_json(&result).unwrap();
     JsValue::from_str(&json)
 }
