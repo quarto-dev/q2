9603cdde535026aa5d6c0b6ef41c298bf94118f8
Carlos Scheidegger <carlos.scheidegger@posit.co>
2025-11-02 08:25:57 -0600

    more cleanup and debugging printout

--- crates/tree-sitter-qmd/tree-sitter-markdown/src/scanner.c
+++ crates/tree-sitter-qmd/tree-sitter-markdown/src/scanner.c
@@ -5,7 +5,7 @@
 #include <wctype.h>
 
 // set this define to turn on debugging printouts
-// #define SCAN_DEBUG 1
+#define SCAN_DEBUG 1
 
 #ifdef SCAN_DEBUG
 #define DEBUG_HERE printf("At line %d\n", __LINE__);
